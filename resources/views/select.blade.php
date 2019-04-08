9a9063b4f429f474eb28dba7729595086389b01a
xiaohui.lam <xiaohui.lam@icloud.com>
2019-04-08 22:59:47 +0800

    resolve #11

--- resources/views/select.blade.php
+++ resources/views/select.blade.php
@@ -1,14 +1,31 @@
-<div class="form-group {!! !$errors->has($label) ?: 'has-error' !!}" >
+<div class="{{$viewClass['form-group']}} {!! !$errors->has($errorKey) ? '' : 'has-error' !!}">
 
-    <label for="{{$id}}" class="col-sm-2 control-label">{{$label}}</label>
-    <div class="col-sm-8">
+<label for="{{$id}}" class="{{$viewClass['label']}} control-label">{{$label}}</label>
+
+    <div class="{{$viewClass['field']}}">
+
         @include('admin::form.error')
 
-        <div class="controls" id="select2-wrapper-{{$name}}">
-            <select name="{{$name}}" id="{{$name}}-select2" class="form-control input-block {{$name}}" data-value="{{ old($column, $value) }}">
-                <option value="{{ old($column, $value) }}" selected>{{ old($column, $value) }}</option>
-            </select>
-        </div>
+        <input type="hidden" name="{{$name}}"/>
+
+        <select class="form-control {{$class}}" style="width: 100%;" name="{{$name}}" {!! $attributes !!} >
+            @if($groups)
+                @foreach($groups as $group)
+                    <optgroup label="{{ $group['label'] }}">
+                        @foreach($group['options'] as $select => $option)
+                            <option value="{{$select}}" {{ $select == old($column, $value) ?'selected':'' }}>{{$option}}</option>
+                        @endforeach
+                    </optgroup>
+                @endforeach
+             @else
+                <option value=""></option>
+                @foreach($options as $select => $option)
+                    <option value="{{$select}}" {{ $select == old($column, $value) ?'selected':'' }}>{{$option}}</option>
+                @endforeach
+            @endif
+        </select>
+
         @include('admin::form.help-block')
+
     </div>
 </div>
