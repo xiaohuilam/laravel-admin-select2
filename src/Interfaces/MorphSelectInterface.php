fdf3a76fb27b43077abd8a45cea6c1e850b63c6b
xiaohui.lam <xiaohui.lam@icloud.com>
2019-04-09 16:56:24 +0800

    add interface

---
+++ src/Interfaces/MorphSelectInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace LaravelAdminExt\Select2\Interfaces;
+
+interface MorphSelectInterface
+{
+    /**
+     * Get text column name which select2 ajax wants
+     *
+     * @return string
+     */
+    public static function getTextColumn();
+}
