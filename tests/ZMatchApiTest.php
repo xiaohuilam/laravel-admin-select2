fb43d9baa4cf3313c90f646617ce985ca61613ab
xiaohui.lam <xiaohui.lam@icloud.com>
2019-04-11 14:59:45 +0800

    debug

--- tests/ZMatchApiTest.php
+++ tests/ZMatchApiTest.php
@@ -51,6 +51,7 @@ class ZMatchApiTest extends AbstractTestCase
     {
         $url = $this->url;
         $response = $this->get($url)->response;
+        echo $response->getContent();
 
         $this->assertEquals(200, $response->getStatusCode());
 
