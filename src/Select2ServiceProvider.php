958f55f3c62af8a1b3f3fda580190e91d8365fa6
xiaohui.lam <xiaohui.lam@icloud.com>
2019-04-11 15:12:12 +0800

    fix

--- src/Select2ServiceProvider.php
+++ src/Select2ServiceProvider.php
@@ -5,9 +5,12 @@ namespace LaravelAdminExt\Select2;
 use Encore\Admin\Admin;
 use Encore\Admin\Form;
 use Illuminate\Support\ServiceProvider;
+use LaravelAdminExt\Select2\Form\Field\Select;
 use LaravelAdminExt\Select2\Form\Field\MorphSelect;
 use LaravelAdminExt\Select2\Form\Field\MultipleSelect;
-use LaravelAdminExt\Select2\Form\Field\Select;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Select2ServiceProvider extends ServiceProvider
 {
@@ -31,5 +34,30 @@ class Select2ServiceProvider extends ServiceProvider
             Form::extend('multipleSelect', MultipleSelect::class);
             Form::extend('morphSelect', MorphSelect::class);
         });
+        $this-> __compactLaravel55();
+    }
+
+    protected function __compactLaravel55()
+    {
+        if (!method_exists(BelongsTo::class, 'getForeignKeyName') && BelongsTo::hasMacro('getForeignKeyName')) {
+            BelongsTo::macro('getForeignKeyName', function () {
+                return $this->foreignKey;
+            });
+        }
+        if (!method_exists(BelongsTo::class, 'getForeignKeyName') && BelongsTo::hasMacro('getForeignKeyName')) {
+            BelongsTo::macro('getForeignKeyName', function () {
+                return $this->foreignKey;
+            });
+        }
+        if (!method_exists(HasOne::class, 'getForeignKeyName') && HasOne::hasMacro('getForeignKeyName')) {
+            HasOne::macro('getForeignKeyName', function () {
+                return $this->foreignKey;
+            });
+        }
+        if (!method_exists(HasMany::class, 'getForeignKeyName') && HasMany::hasMacro('getForeignKeyName')) {
+            HasMany::macro('getForeignKeyName', function () {
+                return $this->foreignKey;
+            });
+        }
     }
 }
