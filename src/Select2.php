4b6dd8a1076ec89e55552161e1279dad71a05a69
Xiaohui Lam <xiaohuilam@users.noreply.github.com>
2020-11-13 12:43:35 +0800

    Update Select2.php

--- src/Select2.php
+++ src/Select2.php
@@ -8,16 +8,4 @@ class Select2 extends Extension
 {
     public $name = 'select2';
     public $views = __DIR__ . '/../resources/views';
-
-    /**
-     * {@inheritdoc}
-     */
-    public function render()
-    {
-        if ('laravel-admin-select2::morph.type' === $this->view) {
-            return str_replace('<!--type-->' . PHP_EOL . '</div>', '', parent::render());
-        }
-
-        return parent::render();
-    }
 }
